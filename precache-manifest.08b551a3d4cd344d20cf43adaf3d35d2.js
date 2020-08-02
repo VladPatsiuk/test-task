f5010a50873df4583857f45c1ca7b73ef4c971c1
VladPatsiuk <vladpatsiuk@gmail.com>
2020-08-02 20:10:22 +0300

    Updates

---
+++ precache-manifest.08b551a3d4cd344d20cf43adaf3d35d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631f0dae08a15bbe1aa495374bc4a787",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "946737866ab6a5f94a70",
+    "url": "/test-task/static/css/main.64fc33d7.chunk.css"
+  },
+  {
+    "revision": "3722d6233a9937707733",
+    "url": "/test-task/static/js/2.fb1befe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task/static/js/2.fb1befe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946737866ab6a5f94a70",
+    "url": "/test-task/static/js/main.7cab90a4.chunk.js"
+  },
+  {
+    "revision": "79892429a743b8954c05",
+    "url": "/test-task/static/js/runtime-main.4af6997d.js"
+  }
+]);
